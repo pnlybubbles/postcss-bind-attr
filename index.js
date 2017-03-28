1725fe1db8ad93a6c66fbcea9c556cd061d9c329
pnlybubbles <pnlybubbles@gmail.com>
2017-03-29 00:01:08 +0900

    chore: migrate to standard from eslint

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-const postcss = require('postcss');
-const parser = require('postcss-selector-parser');
+const postcss = require('postcss')
+const parser = require('postcss-selector-parser')
 
-function transform(attr) {
+function transform (attr) {
   return (selectors) => {
-    const nodes = [];
+    const nodes = []
     selectors.walk((selector) => {
-      if (selector.type !== 'pseudo'
-        && selector.type !== 'selector'
-        && selector.type !== 'nesting'
-        && (!selector.prev() || selector.prev().type === 'combinator')) {
-        nodes.push(selector);
+      if (selector.type !== 'pseudo' &&
+        selector.type !== 'selector' &&
+        selector.type !== 'nesting' &&
+        (!selector.prev() || selector.prev().type === 'combinator')) {
+        nodes.push(selector)
       }
-    });
+    })
     nodes.forEach((node) => {
-      node.parent.insertAfter(node, parser.attribute({attribute: attr}));
-    });
-  };
+      node.parent.insertAfter(node, parser.attribute({attribute: attr}))
+    })
+  }
 }
 
-function isRoot(selector) {
-  let isRoot_ = false;
+function isRoot (selector) {
+  let isRoot_ = false
   parser((selectors) => {
     selectors.walk((selector_) => {
       if (selector_.value === ':root') {
-        isRoot_ = true;
-        return false;
+        isRoot_ = true
+        return false
       }
-    });
-  }).process(selector);
-  return isRoot_;
+    })
+  }).process(selector)
+  return isRoot_
 }
 
-function ancestors(nested, target) {
-  return nested === target
-    || (!!nested.parent
-      && ancestors(nested.parent, target));
+function ancestors (nested, target) {
+  return nested === target ||
+    (!!nested.parent &&
+      ancestors(nested.parent, target))
 }
 
 module.exports = postcss.plugin('postcss-bind-attr', (attr) => {
-  const processor = parser(transform(attr));
+  const processor = parser(transform(attr))
   return (css) => {
-    const roots = [];
+    const roots = []
     css.walkRules((rule) => {
       if (isRoot(rule.selector)) {
-        roots.push(rule);
+        roots.push(rule)
       } else {
         if (!roots.some((elt) => ancestors(rule, elt))) {
-          rule.selector = processor.process(rule.selector).result;
+          rule.selector = processor.process(rule.selector).result
         }
       }
-    });
-  };
-});
+    })
+  }
+})
 
-module.exports.transform = transform;
-module.exports.isRoot = isRoot;
-module.exports.ancestors = ancestors;
+module.exports.transform = transform
+module.exports.isRoot = isRoot
+module.exports.ancestors = ancestors
--- test/index.js
+++ test/index.js
@@ -1,214 +1,214 @@
-const bindAttr = require('../');
-const transform = bindAttr.transform;
-const isRoot = bindAttr.isRoot;
-const ancestors = bindAttr.ancestors;
-const postcss = require('postcss');
-const parser = require('postcss-selector-parser');
-const test = require('tape');
-const path = require('path');
-const fs = require('fs');
+const bindAttr = require('../')
+const transform = bindAttr.transform
+const isRoot = bindAttr.isRoot
+const ancestors = bindAttr.ancestors
+const postcss = require('postcss')
+const parser = require('postcss-selector-parser')
+const test = require('tape')
+const path = require('path')
+const fs = require('fs')
 
 test('tag without postcss', (t) => {
-  const attr = 'bound-attr';
-  const expected = `a[${attr}]`;
-  const css = 'a';
-  const out = parser(transform(attr)).process(css).result;
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `a[${attr}]`
+  const css = 'a'
+  const out = parser(transform(attr)).process(css).result
+  t.equal(out, expected)
+  t.end()
+})
 
 test('tag', (t) => {
-  const attr = 'bound-attr';
-  const expected = `a[${attr}] {}`;
-  const css = 'a {}';
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `a[${attr}] {}`
+  const css = 'a {}'
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('multiple tags without postcss', (t) => {
-  const attr = 'bound-attr';
-  const expected = `h1[${attr}] h2[${attr}] h3[${attr}]`;
-  const css = 'h1 h2 h3';
-  const out = parser(transform(attr)).process(css).result;
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `h1[${attr}] h2[${attr}] h3[${attr}]`
+  const css = 'h1 h2 h3'
+  const out = parser(transform(attr)).process(css).result
+  t.equal(out, expected)
+  t.end()
+})
 
 test('multiple tags', (t) => {
-  const attr = 'bound-attr';
-  const expected = `h1[${attr}] h2[${attr}] h3[${attr}] {}`;
-  const css = 'h1 h2 h3 {}';
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `h1[${attr}] h2[${attr}] h3[${attr}] {}`
+  const css = 'h1 h2 h3 {}'
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('group tags without postcss', (t) => {
-  const attr = 'bound-attr';
-  const expected = `h1[${attr}], h2[${attr}], h3[${attr}]`;
-  const css = 'h1, h2, h3';
-  const out = parser(transform(attr)).process(css).result;
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `h1[${attr}], h2[${attr}], h3[${attr}]`
+  const css = 'h1, h2, h3'
+  const out = parser(transform(attr)).process(css).result
+  t.equal(out, expected)
+  t.end()
+})
 
 test('group tags', (t) => {
-  const attr = 'bound-attr';
-  const expected = `h1[${attr}], h2[${attr}], h3[${attr}] {}`;
-  const css = 'h1, h2, h3 {}';
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `h1[${attr}], h2[${attr}], h3[${attr}] {}`
+  const css = 'h1, h2, h3 {}'
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('ids classes attributes without postcss', (t) => {
-  const attr = 'bound-attr';
-  const expected = `#id[${attr}] .class[${attr}] [attr][${attr}]`;
-  const css = '#id .class [attr]';
-  const out = parser(transform(attr)).process(css).result;
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `#id[${attr}] .class[${attr}] [attr][${attr}]`
+  const css = '#id .class [attr]'
+  const out = parser(transform(attr)).process(css).result
+  t.equal(out, expected)
+  t.end()
+})
 
 test('ids classes attributes', (t) => {
-  const attr = 'bound-attr';
-  const expected = `#id[${attr}] .class[${attr}] [attr][${attr}] {}`;
-  const css = '#id .class [attr] {}';
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `#id[${attr}] .class[${attr}] [attr][${attr}] {}`
+  const css = '#id .class [attr] {}'
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('multiple conditions without postcss', (t) => {
-  const attr = 'bound-attr';
-  const expected = `#id[${attr}].class[attr]`;
-  const css = '#id.class[attr]';
-  const out = parser(transform(attr)).process(css).result;
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `#id[${attr}].class[attr]`
+  const css = '#id.class[attr]'
+  const out = parser(transform(attr)).process(css).result
+  t.equal(out, expected)
+  t.end()
+})
 
 test('multiple conditions', (t) => {
-  const attr = 'bound-attr';
-  const expected = `#id[${attr}].class[attr] {}`;
-  const css = '#id.class[attr] {}';
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `#id[${attr}].class[attr] {}`
+  const css = '#id.class[attr] {}'
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('multiple lines', (t) => {
-  const attr = 'bound-attr';
+  const attr = 'bound-attr'
   const expected = `h1[${attr}] {
   }
   h2[${attr}] {
-  }`;
+  }`
   const css = `h1 {
   }
   h2 {
-  }`;
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  }`
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('nested selectors', (t) => {
-  const attr = 'bound-attr';
+  const attr = 'bound-attr'
   const expected = `h1[${attr}] {
     & h2[${attr}] {
     }
-  }`;
+  }`
   const css = `h1 {
     & h2 {
     }
-  }`;
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  }`
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('media query', (t) => {
-  const attr = 'bound-attr';
+  const attr = 'bound-attr'
   const expected = `@media screen and (max-width: 100px) {
     a[${attr}] {}
-  }`;
+  }`
   const css = `@media screen and (max-width: 100px) {
     a {}
-  }`;
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  }`
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('pseudo without postcss', (t) => {
-  const attr = 'bound-attr';
-  const expected = `h1[${attr}]:hover h2[${attr}]::after`;
-  const css = 'h1:hover h2::after';
-  const out = parser(transform(attr)).process(css).result;
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `h1[${attr}]:hover h2[${attr}]::after`
+  const css = 'h1:hover h2::after'
+  const out = parser(transform(attr)).process(css).result
+  t.equal(out, expected)
+  t.end()
+})
 
 test('pseudo', (t) => {
-  const attr = 'bound-attr';
-  const expected = `h1[${attr}]:hover h2[${attr}]::after {}`;
-  const css = 'h1:hover h2::after {}';
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = `h1[${attr}]:hover h2[${attr}]::after {}`
+  const css = 'h1:hover h2::after {}'
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('ignore root without postcss', (t) => {
-  const attr = 'bound-attr';
-  const expected = ':root';
-  const css = ':root';
-  const out = parser(transform(attr)).process(css).result;
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = ':root'
+  const css = ':root'
+  const out = parser(transform(attr)).process(css).result
+  t.equal(out, expected)
+  t.end()
+})
 
 test('ignore root', (t) => {
-  const attr = 'bound-attr';
-  const expected = ':root {}';
-  const css = ':root {}';
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  const attr = 'bound-attr'
+  const expected = ':root {}'
+  const css = ':root {}'
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('isRoot', (t) => {
-  t.equal(isRoot(':root'), true, 'root');
-  t.equal(isRoot(':root a'), true, 'nested root');
-  t.equal(isRoot('a'), false, 'no root');
-  t.end();
-});
+  t.equal(isRoot(':root'), true, 'root')
+  t.equal(isRoot(':root a'), true, 'nested root')
+  t.equal(isRoot('a'), false, 'no root')
+  t.end()
+})
 
 test('ancestors', (t) => {
-  const part = {parent: {}};
-  t.equal(ancestors(part, part), true, 'same');
-  t.equal(ancestors({parent: part}, part), true, 'include part');
-  t.equal(ancestors({parent: {parent: {}}}, part), false, 'not include part');
-  t.end();
-});
+  const part = {parent: {}}
+  t.equal(ancestors(part, part), true, 'same')
+  t.equal(ancestors({parent: part}, part), true, 'include part')
+  t.equal(ancestors({parent: {parent: {}}}, part), false, 'not include part')
+  t.end()
+})
 
 test('ignore root (nested)', (t) => {
-  const attr = 'bound-attr';
+  const attr = 'bound-attr'
   const expected = `:root a {
     & .nested {}
-  }`;
+  }`
   const css = `:root a {
     & .nested {}
-  }`;
-  const out = postcss().use(bindAttr(attr)).process(css).toString();
-  t.equal(out, expected);
-  t.end();
-});
+  }`
+  const out = postcss().use(bindAttr(attr)).process(css).toString()
+  t.equal(out, expected)
+  t.end()
+})
 
 test('generally file', (t) => {
-  const expected = fs.readFileSync(path.resolve(__dirname, 'fixture-out.css'), 'utf8');
-  const css = fs.readFileSync(path.resolve(__dirname, 'fixture.css'), 'utf8');
-  const out = postcss().use(bindAttr('bound-attr')).process(css).toString();
-  t.equal(out, expected, 'output is expected');
-  t.end();
-});
+  const expected = fs.readFileSync(path.resolve(__dirname, 'fixture-out.css'), 'utf8')
+  const css = fs.readFileSync(path.resolve(__dirname, 'fixture.css'), 'utf8')
+  const out = postcss().use(bindAttr('bound-attr')).process(css).toString()
+  t.equal(out, expected, 'output is expected')
+  t.end()
+})
