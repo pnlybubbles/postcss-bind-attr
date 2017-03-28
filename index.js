982fbfc2c788faed362db293b8838d58dda2b9f1
pnlybubbles <pnlybubbles@gmail.com>
2017-03-28 22:20:00 +0900

    feat: not cascading from root even if nested

--- index.js
+++ index.js
@@ -18,13 +18,41 @@ function transform(attr) {
   };
 }
 
+function isRoot(selector) {
+  let isRoot_ = false;
+  parser((selectors) => {
+    selectors.walk((selector_) => {
+      if (selector_.value === ':root') {
+        isRoot_ = true;
+        return false;
+      }
+    });
+  }).process(selector);
+  return isRoot_;
+}
+
+function ancestors(nested, target) {
+  return nested === target
+    || (!!nested.parent
+      && ancestors(nested.parent, target));
+}
+
 module.exports = postcss.plugin('postcss-bind-attr', (attr) => {
   const processor = parser(transform(attr));
   return (css) => {
+    const roots = [];
     css.walkRules((rule) => {
-      rule.selector = processor.process(rule.selector).result;
+      if (isRoot(rule.selector)) {
+        roots.push(rule);
+      } else {
+        if (!roots.some((elt) => ancestors(rule, elt))) {
+          rule.selector = processor.process(rule.selector).result;
+        }
+      }
     });
   };
 });
 
 module.exports.transform = transform;
+module.exports.isRoot = isRoot;
+module.exports.ancestors = ancestors;
--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 const bindAttr = require('../');
 const transform = bindAttr.transform;
+const isRoot = bindAttr.isRoot;
+const ancestors = bindAttr.ancestors;
 const postcss = require('postcss');
 const parser = require('postcss-selector-parser');
 const test = require('tape');
@@ -139,10 +141,74 @@ test('media query', (t) => {
   t.end();
 });
 
-// test('postcss-bind-attr', (t) => {
-//   const expected = fs.readFileSync(path.resolve(__dirname, 'fixture-out.css'), 'utf8');
-//   const css = fs.readFileSync(path.resolve(__dirname, 'fixture.css'), 'utf8');
-//   const out = postcss().use(bindAttr('bound-attr')).process(css).toString();
-//   t.equal(expected, out, 'output is expected');
-//   t.end();
-// });
+test('pseudo without postcss', (t) => {
+  const attr = 'bound-attr';
+  const expected = `h1[${attr}]:hover h2[${attr}]::after`;
+  const css = 'h1:hover h2::after';
+  const out = parser(transform(attr)).process(css).result;
+  t.equal(out, expected);
+  t.end();
+});
+
+test('pseudo', (t) => {
+  const attr = 'bound-attr';
+  const expected = `h1[${attr}]:hover h2[${attr}]::after {}`;
+  const css = 'h1:hover h2::after {}';
+  const out = postcss().use(bindAttr(attr)).process(css).toString();
+  t.equal(out, expected);
+  t.end();
+});
+
+test('ignore root without postcss', (t) => {
+  const attr = 'bound-attr';
+  const expected = ':root';
+  const css = ':root';
+  const out = parser(transform(attr)).process(css).result;
+  t.equal(out, expected);
+  t.end();
+});
+
+test('ignore root', (t) => {
+  const attr = 'bound-attr';
+  const expected = ':root {}';
+  const css = ':root {}';
+  const out = postcss().use(bindAttr(attr)).process(css).toString();
+  t.equal(out, expected);
+  t.end();
+});
+
+test('isRoot', (t) => {
+  t.equal(isRoot(':root'), true, 'root');
+  t.equal(isRoot(':root a'), true, 'nested root');
+  t.equal(isRoot('a'), false, 'no root');
+  t.end();
+});
+
+test('ancestors', (t) => {
+  const part = {parent: {}};
+  t.equal(ancestors(part, part), true, 'same');
+  t.equal(ancestors({parent: part}, part), true, 'include part');
+  t.equal(ancestors({parent: {parent: {}}}, part), false, 'not include part');
+  t.end();
+});
+
+test('ignore root (nested)', (t) => {
+  const attr = 'bound-attr';
+  const expected = `:root a {
+    & .nested {}
+  }`;
+  const css = `:root a {
+    & .nested {}
+  }`;
+  const out = postcss().use(bindAttr(attr)).process(css).toString();
+  t.equal(out, expected);
+  t.end();
+});
+
+test('generally file', (t) => {
+  const expected = fs.readFileSync(path.resolve(__dirname, 'fixture-out.css'), 'utf8');
+  const css = fs.readFileSync(path.resolve(__dirname, 'fixture.css'), 'utf8');
+  const out = postcss().use(bindAttr('bound-attr')).process(css).toString();
+  t.equal(out, expected, 'output is expected');
+  t.end();
+});
